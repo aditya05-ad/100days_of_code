a958ad5faae0be59e68a7aa2c8026039125390fb
aditya05-ad <adityadudi05@gmail.com>
2025-11-27 22:32:36 +0530

    Find the kth smallest element in an array
    This program finds the k-th smallest element in an integer array.

---
+++ day63_Q113.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void sort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[i]) {
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+
+    sort(arr, n);
+
+    printf("%d\n", arr[k - 1]);
+
+    return 0;
+}
